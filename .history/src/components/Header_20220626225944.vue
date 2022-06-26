05b37ebec9b2097583cd782a0611a3973d1cd076
Mahmoud Eid <mahmoud.eid@code95.com>
2022-06-26 23:04:04 +0200

    first commit

---
+++ .history/src/components/Header_20220626225944.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="../assets/logo.png"/>
+</template>
+
+<script>
+export default {
+  name: 'Nav'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
